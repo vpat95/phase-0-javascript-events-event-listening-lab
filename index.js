9afae20e0d91d9ae3fcfdceb62c43b13ca27b1a8
Vivek Patel <vpatel360@gmail.com>
2022-08-06 12:04:51 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+const input = document.getElementById('button');
+
 function addingEventListener() {
+
+    function clickAlert() {
+        alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+addingEventListener(input);
